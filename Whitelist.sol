7302c3034e20c2c8c5224872cc462019c82ff493
Uno <96509918+UnoOn@users.noreply.github.com>
2022-01-05 01:25:29 +0900

    add whitelist

--- Whitelist.sol
+++ Whitelist.sol
@@ -1,1 +1,22 @@
-.
+pragma solidity ^0.5.6;
+
+import "./ownership/Ownable.sol";
+
+contract Whitelist is Ownable {
+    mapping(address => bool) public isWhitelist;
+    uint256 public totalNum;
+    address[] public whitelist;
+    bool public whitelistIsActive = true;
+
+    function putIn() external {
+        require(whitelistIsActive);
+        require(!isWhitelist[msg.sender]);
+        isWhitelist[msg.sender] = true;
+        totalNum += 1;
+        whitelist.push(msg.sender);
+    }
+
+    function setWhitelistState(bool newState) public onlyOwner {
+        whitelistIsActive = newState;
+    }
+}
